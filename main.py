19af0c1316bbf0e9433cad93a36c604d0212525c
ODAIR J.  ROSARIO <84051161+OdairRos@users.noreply.github.com>
2021-06-18 21:38:10 -0300

    Começando desenvolver a parte gráfica com Tkinter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from os import *
 from moviepy.editor import *
 from sys import *
+import app as application
 
 EXTENSION_VIDEOS ={
     #   'extensão': 'codec'
@@ -47,39 +48,4 @@ elif tipo == '3' or tipo == 'MKV': Projeto.ConvertVideo('.mkv')
 elif tipo == '4' or tipo == 'OGV': Projeto.ConvertVideo('.ogv')
 elif tipo == '5' or tipo == 'WEBM': Projeto.ConvertVideo('.webm')
 elif tipo == '6' or tipo == 'MP3': Projeto.ConvertVideoToAudio()
-
-
-"""
-codigo antigo: 
-
-
-try:
-    Arquivo = input('Local do arquivo: ')
-    Video = VideoFileClip(Arquivo)
-    NOME_ARQUIVO = os.path.basename(Arquivo).split('.')
-except:
-    print('Arquivo não encontrado, verefique se digitou o local correto\n')
-
-
-
-if tipo == '1' or tipo == 'MP4':
-    Video.write_videofile(NOME_ARQUIVO[0]+'.mp4', codec=EXTENSION_VIDEOS['.mp4']) # Esta linha esta convertendo o arquivo para o formato desejado, usando o codec correto.
-elif tipo == '2' or tipo == 'AVI':
-    Video.write_videofile(NOME_ARQUIVO[0]+'.avi', codec=EXTENSION_VIDEOS['.avi'])
-elif tipo == '3' or tipo == 'MKV':
-    Video.write_videofile(NOME_ARQUIVO[0]+'.mkv', codec = EXTENSION_VIDEOS['.mkv'])
-elif tipo == '4' or tipo == 'OGV':
-    Video.write_videofile(NOME_ARQUIVO[0]+'.ogv', codec=EXTENSION_VIDEOS['.mkv'])
-elif tipo == '5' or tipo == 'WEBM':
-    Video.write_videofile(NOME_ARQUIVO[0]+'.webm', codec=EXTENSION_VIDEOS['.wemb'])
-elif  tipo == '6' or tipo == 'MP3':
-    Audio = Video.audio
-    Audio.write_audiofile(f'{NOME_ARQUIVO[0]}.mp3')
-    Audio.close()
-
-Video.close()
-"""
-
-
-
 
